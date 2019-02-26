7f92591dd256fa875853559a39bd7b5b0f1f0f8f
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-02-26 14:19:00 +0300

    wip

--- similarity.js
+++ similarity.js
@@ -1,9 +1,11 @@
-const checkLength = (s1, s2) => {
+//www.codewars.com/kata/t-dot-t-t-dot-29-similarity-evaluation-of-two-strings/train/javascript
+var checkLength = (s1, s2) => {
   if (s1.length === s2.length) {
     return 10;
   }
   return 0;
 };
+
 const checkFirstAndLastLetter = (s1, s2) => {
   if (s1.length >= 2 && s2.length >= 2) {
     if (s1[0] === s2[0]) {
@@ -24,29 +26,36 @@ const checkSameNumOfLetters = (s1, s2) => {
   return 0;
 };
 const checkSameNumOfNums = (s1, s2) => {
-  s1 = s1.replace(/[A-Za-z]/gi, "");
-  s2 = s2.replace(/[A-Za-z]/gi, "");
+  s1 = s1.replace(/\D/gi, "");
+  s2 = s2.replace(/\D/gi, "");
+  if (s1.length === s2.length) {
+    return 5;
+  }
+  return 0;
+};
+const checkNumOfWords = (s1, s2) => {
+  s1 = s1.split(" ");
+  s2 = s2.split(" ");
+
+  if (s1.length <= 1 && s2.length <= 1) {
+    return 0;
+  }
   if (s1.length === s2.length) {
     return 5;
   }
   return 0;
 };
 
-const checkNumOfWords = (s1, s2) => {
-  s1 = s1.split(" ");
-  s2 = s2.split(" ");
-  const flag = true;
-  s1.forEach(element => {
-    if (s1.indexOf(element) === -1) {
-      flag = false;
-    }
-  });
-  return flag;
-};
 const checkAscending = arr => {
   return arr
     .slice(1)
     .map((e, i) => e > arr[i])
+    .every(x => x);
+};
+const checkDescending = arr => {
+  return arr
+    .slice(1)
+    .map((e, i) => e < arr[i])
     .every(x => x);
 };
 const checkAscii = (s1, s2) => {
@@ -58,10 +67,26 @@ const checkAscii = (s1, s2) => {
     acc.push(elm.charCodeAt());
     return acc;
   }, []);
-  s1 = checkAscending(s1);
-  s2 = checkAscending(s2);
-  if (s1 && s2) {
+  let ascending1 = checkAscending(s1);
+  let ascending2 = checkAscending(s2);
+  let decending1 = checkDescending(s1);
+  let decending2 = checkDescending(s2);
+  if ((ascending1 && ascending2) || (decending1 && decending2)) {
     return 10;
+  }
+  return 0;
+};
+const checkAsciiSum = (s1, s2) => {
+  s1 = s1.split("").reduce((acc, elm) => {
+    acc += elm.charCodeAt();
+    return acc;
+  }, 0);
+  s2 = s2.split("").reduce((acc, elm) => {
+    acc += elm.charCodeAt();
+    return acc;
+  }, 0);
+  if (s1 === s2) {
+    return 20;
   }
   return 0;
 };
@@ -79,28 +104,23 @@ const checkReverseCase = (s1, s2) => {
 };
 
 const checkPalindrome = (s1, s2) => {
-  let palindrome1 = s1.split("").filter((elm, index, arr) => {
-    if (elm === arr[arr.length - index - 1]) {
-      return true;
-    }
-    return false;
-  });
-  let palindrome2 = s2.split("").filter((elm, index, arr) => {
-    if (elm === arr[arr.length - index - 1]) {
-      return true;
-    }
-    return false;
-  });
-  if (palindrome1.length > 1 && palindrome2.length > 1) {
+  const palidrome1 = s1
+    .split("")
+    .reverse()
+    .join("");
+  const palidrome2 = s2
+    .split("")
+    .reverse()
+    .join("");
+  if (palidrome1 === s1 && palidrome2 === s2) {
     return 10;
   }
   return 0;
 };
 
 const identicalChar = (s1, s2) => {
-  s1 = Array.from(new Set(s1.split("")));
-  s2 = Array.from(new Set(s2.split("")));
-  if (s1.length === 1 && s2.length === 1) {
+  const setOfBoth = Array.from(new Set(s1.concat(...s2)));
+  if (setOfBoth.length === 1) {
     if (s1[0] === s2[0]) {
       return 40;
     }
@@ -109,16 +129,108 @@ const identicalChar = (s1, s2) => {
 };
 
 const checkDifferenceOfCHars = (s1, s2) => {
-  s1 = Array.from(new Set(s1.split("").sort()));
-  s2 = Array.from(new Set(s2.split("").sort()));
-  const res = s1.filter(elm => s2.indexOf(elm) == -1);
+  let res = [];
+  s1 = s1.split("");
+  s2 = s2.split("");
+  s1.forEach(elm => {
+    if (s2.indexOf(elm) === -1) {
+      res.push(elm);
+    }
+  });
   if (res.length === 1) {
     return 30;
   }
   return 0;
 };
-checkReverseCase("abc", "cba");
+
+const containSameLetters = (s1, s2) => {
+  s1 = s1.replace(/\W/gi, "").split("");
+  s2 = s2.replace(/\W/gi, "").split("");
+  const lettersFromBothArrs = Array.from(new Set(s1.concat(s2)));
+  let counter = 0;
+  lettersFromBothArrs.forEach(elm => {
+    if (s1.indexOf(elm) >= 0 && s2.indexOf(elm) >= 0) {
+      counter += 2;
+    }
+  });
+  if (counter > 10) {
+    return 10;
+  } else if (counter <= 10) {
+    return counter;
+  }
+};
+const containSameNumbers = (s1, s2) => {
+  s1 = s1.replace(/\D/gi, "").split("");
+  s2 = s2.replace(/\D/gi, "").split("");
+  const lettersFromBothArrs = Array.from(new Set(s1.concat(s2)));
+  let counter = 0;
+  lettersFromBothArrs.forEach(elm => {
+    if (s1.indexOf(elm) >= 0 && s2.indexOf(elm) >= 0) {
+      counter += 2;
+    }
+  });
+  if (counter > 10) {
+    return 10;
+  } else if (counter <= 10) {
+    return counter;
+  }
+};
+
+const containOtherChars = (s1, s2) => {
+  let count = 0;
+  s1 = s1
+    .replace(/\w/gi, "")
+    .split("")
+    .sort();
+  s2 = s2
+    .replace(/\w/gi, "")
+    .split("")
+    .sort();
+  s1.forEach(elm => {
+    if (s2.indexOf(elm) >= 0) {
+      count += 4;
+      s2.splice(s2.indexOf(elm), 1);
+    }
+  });
+  if (count >= 20) {
+    return 20;
+  }
+  return count;
+};
+
+const checkIfSame = (s1, s2) => {
+  return s1 === s2;
+};
+
 function similarityEvaluation(s1, s2) {
-  //coding here...
+  let counter = 0;
+  if (checkIfSame(s1, s2)) {
+    return 100;
+  }
+  if (s1.length === 0 || s2.length === 0) {
+    return 0;
+  }
+  const criteria = [
+    checkLength,
+    checkFirstAndLastLetter,
+    checkSameNumOfLetters,
+    checkSameNumOfNums,
+    checkNumOfWords,
+    checkAscii,
+    checkAsciiSum,
+    checkReverseCase,
+    checkPalindrome,
+    identicalChar,
+    checkDifferenceOfCHars,
+    containSameLetters,
+    containSameNumbers,
+    containOtherChars
+  ];
+  criteria.forEach(elm => {
+    let res = elm(s1, s2);
+    console.log(elm, res);
+    counter += res;
+  });
+  console.log(counter);
 }
-similarityEvaluation("^ptbA9oD*tlFWilGWLu", "rqrx&RSsKUciXXz?p}p");
+console.log(similarityEvaluation("111AAA     AAA111", "111AAA      AAA111"));
