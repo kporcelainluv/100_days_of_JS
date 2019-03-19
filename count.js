a52e840e4d81b96db351322cf348f7f4f7e2f993
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-03-19 12:47:43 +0300

    41

--- count.js
+++ count.js
@@ -1,9 +1,7 @@
 const sequenceSum = (begin, end, step) => {
   let res = 0;
   for (let i = begin; i <= end; i += step) {
-    console.log(i);
     res += i;
-    console.log({ res });
   }
   return res;
 };
